0652ba75876eb045e51829f79969168c9974138e
Wiliam Souza <wiliamsouza83@gmail.com>
2014-05-23 23:24:51 -0300

    Fix repos typo

--- repos.sh
+++ repos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git remote add github git@github.com:wiliamsouza/docker-localnet.git
-git remote add bitbucket git@bitbucket.org:wiliamsouza/docker-localnet.git
-git remote add gitlab git@gitlab.com:wiliamsouza/docker-localnet.git
-git remote set-url --push --add origin git@bitbucket.org:wiliamsouza/docker-localnet.git
-git remote set-url --push --add origin git@gitlab.com:wiliamsouza/docker-localnet.git
-git remote set-url --push --add origin git@github.com:wiliamsouza/docker-localnet.git
+git remote add github git@github.com:wiliamsouza/localnet.git
+git remote add bitbucket git@bitbucket.org:wiliamsouza/localnet.git
+git remote add gitlab git@gitlab.com:wiliamsouza/localnet.git
+git remote set-url --push --add origin git@bitbucket.org:wiliamsouza/localnet.git
+git remote set-url --push --add origin git@gitlab.com:wiliamsouza/localnet.git
+git remote set-url --push --add origin git@github.com:wiliamsouza/localnet.git
